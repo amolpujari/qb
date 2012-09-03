ab3de36627256f588aea7cb78d6826ac4798e2fa
Amol Pujari <amol.pujari@gslab.com>
2012-09-03 16:41:25 +0530

    resolve bootstrap file not found issue with production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Qb::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -61,7 +61,7 @@ Qb::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'young-meadow-8539.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => '172.20.25.187:5678/' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
