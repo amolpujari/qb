c793a79c640a83758e95a52d65f4046a1ec787a1
Amol Pujari <amolpujari@gmail.com>
2013-01-20 00:28:50 +0530

    removed rescue nil

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
   def corrected_search_filter
     return @search_filter if @search_filter
-    @search_filter = params[:search].to_s.strip rescue nil
+    @search_filter = params[:search].to_s.strip unless params[:search].blank?
     return nil unless @search_filter
     @search_filter = "#{@search_filter}*" unless @search_filter.strip.match /.*[\*]$/
     @search_filter = "*#{@search_filter}" unless @search_filter.strip.match /^[\*].*/
