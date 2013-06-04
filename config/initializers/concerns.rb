256dc4d636cac5542be9fd486b0dda98423d881d
Amol Pujari <amolpujari@gmail.com>
2013-06-04 12:45:06 +0530

    adding normal module as concerns

--- config/initializers/concerns.rb
+++ config/initializers/concerns.rb
@@ -2,6 +2,8 @@ class << ActiveRecord::Base
   def concerned_with(*concerns)
     concerns.each do |concern|
       require_dependency "#{name.underscore}/#{concern}"
+      klass = concern.to_s.classify.constantize rescue nil
+      send(:include, klass) if klass.is_a? Module
     end
   end
 end
